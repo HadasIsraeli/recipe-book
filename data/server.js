d862627ed539416025640ef7240f5a2fba8bd2f5
HADASISRAELI\hadas <hadas25496@gmail.com>
2023-06-04 13:35:34 +0300

    for deploying

--- data/server.js
+++ data/server.js
@@ -3,18 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const recipesRoutes = require('./routes/recipes');
+const cors = require('cors');
+
 
 const app = express();
 
-// const Recipe = require('./models/recipesModel');
-
-
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "https://task-deloitte.netlify.app"
+        ],
+    })
+);
+
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method);
+//     next();
+// })
 
 app.use('/api/recipes', recipesRoutes);
 
@@ -36,14 +44,3 @@ mongoose.connect(process.env.URI_MONGO)
 app.get('/', (req, res) => {
     res.json({ mssg: "welcome to the api" });
 });
-
-// app.post('/uploads', async (req, res) => {
-//     const upload = req.body;
-//     try {
-//         const uploadImg = await Recipe.create(upload);
-//         uploadImg.save();
-//         res.status(201).json({ msg: "new img" })
-//     } catch (err) {
-//         res.status(409).json({ msg: "error creating img" });
-//     }
-// })
